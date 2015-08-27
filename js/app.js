a7c4b6c2c794d97ad8046433aa95807bac10bad1
Jason Arnold <jason@thejasonfile.com>
2015-08-27 17:56:13 -0400

    changed design and adding some JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-	$("#subtotal").keyup(function() {
-		var billAmount = $("#subtotal").val();
-		console.log(billAmount);
-	}); //end of keyup
-}); //end of ready
+	$("#15per").click(function() {
+	var tip = $("#subtotal").val() * 0.15;
+	$(".tipamount").html("<p>" + tip + "</p>")
+	$(".totalamount").html("<p>")
+	});
+
+	$("#20per").click(function() {
+	var tip = $("#subtotal").val() * 0.20;
+	$(".tipamount").html("<p>" + Total + "</p>")
+	});
+
+	$("#25per").click(function() {
+	var Total = $("#subtotal").val() * 0.25;
+	$(".tipamount").html("<p>" + Total + "</p>")
+	});	
+
+})
+	
